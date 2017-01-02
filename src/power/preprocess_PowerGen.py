47b8a67241dcda0ab2dc8301e223cd25b41e9512
ben811016x <xx67guy@yahoo.com.tw>
2017-01-02 14:49:45 +0800

    Remove files

--- src/power/preprocess_PowerGen.py
+++ src/power/preprocess_PowerGen.py
@@ -1,34 +1,42 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Dec 17 14:37:42 2016
-
 @author: Ben
 """
 
 from collections import defaultdict
+import glob
 import MySQLdb
 import time
 import sys
 import json
+import os
 import unicodecsv as csv
 
-FILE_NAMES = ['C:/Users/Ben/Desktop/power_data/2016-12-14-02_50_01.txt'
 
-              
-              
-              
-              ]
 
-for fileName in FILE_NAMES:
-    rawlogfile = open(fileName, "r")
-    data=rawlogfile.read().replace('\n', '')
-    decodedjson =  json.loads(data)
-    D=decodedjson.get("aaData")
+
+
+FILE_NAMES=[]
+LOG_PATH = 'C:/Users/Ben/Desktop/power_data'
+FILE_NAMES = glob.glob(os.path.join(LOG_PATH, "*.txt"))
+
+with open('temp.csv', 'wb') as csvfile:
+    for fileName in FILE_NAMES:
+        print(fileName)
+        rawlogfile = open(fileName, "r")
+        
+        data=rawlogfile.read().replace('\n', '')
+
+
+        decodedjson =  json.loads(data)
+        D=decodedjson.get("aaData")
+        
+        count=0
     
-    count=0
-    with open(decodedjson.get("").replace(':', '_') + '.csv', 'wb') as csvfile:
+        T=decodedjson.get("").replace('-', '/');
         writer = csv.writer(csvfile)
-        writer.writerow(['PowerType',u"機組名稱",u"裝置容量(千瓩)",u"淨發電量(千瓩)",u"淨發電量/裝置容量比",u"備註"])
+        #writer.writerow(['PowerType',u"機組名稱",u"裝置容量(千瓩)",u"淨發電量(千瓩)",u"淨發電量/裝置容量比",u"備註"])
         while count<len(D): 
     
             if D[count][1].find(u"小計") ==-1:
@@ -40,6 +48,7 @@ for fileName in FILE_NAMES:
                 delete=name[name.find('('):name.find(')')+1]
                 if delete!='':
                     name=name.replace(delete,"")
+                
                          
                 capacity=D[count][2]
                 if capacity.find('-')!=-1:
@@ -59,8 +68,7 @@ for fileName in FILE_NAMES:
                     
                 comment=D[count][5]
                 
-                if powertype !='Pumping Load' and powertype !='Pumping Gen':
-                    print powertype+","+name+","+capacity+","+netpowergen+","+percent+","+comment
-                    writer.writerow([powertype,name,capacity,netpowergen,percent,comment])
+                if powertype !='Pumping Load' and powertype !='Pumping Gen' and name !=u'汽電共生' and powertype !='Wind' and powertype !='Solar':
+                    #print powertype+","+name+","+capacity+","+netpowergen+","+percent+","+T
+                    writer.writerow([powertype,name,capacity,netpowergen,percent,T])
             count+=1
-         
